d621c15e799226a5d2ba2340de94aaf4d3533278
Shayaan Amin <sa08774@st.habib.edu.pk>
2025-10-03 10:49:28 +0500

    lab 6

---
+++ src/numpy.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class numpy{
+    private:
+        int arr[10];
+    public:
+    numpy(){
+        for (int i=0; i<10; i++){
+            arr[i]=0
+        }
+    }
+    void input(){
+        for (int i=0; i<10;i++){
+            cin>>arr[i]
+        }
+    }
+
+}
